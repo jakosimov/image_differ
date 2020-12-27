5154746abfb1294c51b104394d68e927b6785ded
Eruvarn <jakobalwall@gmail.com>
2020-12-27 04:01:32 +0100

    Hmmm.

--- main.py
+++ main.py
@@ -3,8 +3,9 @@ import numpy as np
 import threading
 from copy import deepcopy
 from cv2 import VideoWriter, VideoWriter_fourcc, VideoCapture
+from collections import deque
 
-FPS = 5
+FPS = 5 
 
 vidcap = VideoCapture('video.mp4')
 success, image = vidcap.read()
@@ -13,26 +14,29 @@ height = int(vidcap.get(3))
 fourcc = VideoWriter_fourcc(*'XVID')
 videoWriter = VideoWriter('output.avi', fourcc, float(FPS), (height, width))
 count = 0
-max_count = 20000
-last_frames = []
+max_count = 100
+last_frames = deque()
+frames_to_save = 30 
+running_total = np.full(width * height * 3, 0)
 
-number_of_threads = 1
-thread_span = int(width / number_of_threads)
 print('width =', width)
 print('height =', height)
 
 def calculate_frame(image):
-    global last_frames
+    global last_frames, running_total
     image = np.matrix.flatten(image)
-    image = np.int8(image)
+    image = np.int32(image)
     image_copy = deepcopy(image)
-    if len(last_frames) >= 1:
-        last_frame = last_frames[-1]
-        image = np.absolute(np.subtract(image, last_frame))
-        last_frames = last_frames[1:]
+    if len(last_frames) >= frames_to_save:
+        average_frame = running_total / frames_to_save
+        image = np.absolute(np.subtract(image, average_frame))
+        first_frame = last_frames.popleft()
+        running_total = np.subtract(running_total, first_frame)
     last_frames.append(image_copy)
+    running_total = np.add(running_total, image_copy)
     image = np.uint8(image)
     image = np.resize(image, (width, height, 3)) 
+    # print(image)
     videoWriter.write(image)
 
 while success and count < max_count:
