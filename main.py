9020b7ca1ec1ce4f40e4918081b45fc3e9880cb6
Eruvarn <jakobalwall@gmail.com>
2020-12-27 03:02:55 +0100

    ??

--- main.py
+++ main.py
@@ -4,23 +4,7 @@ import threading
 from copy import deepcopy
 from cv2 import VideoWriter, VideoWriter_fourcc, VideoCapture
 
-def pixel_difference(pix1, pix2):
-    if pix1[0] > pix2[0]:
-        pix1[0] = pix1[0] - pix2[0]
-    else:
-        pix1[0] = pix2[0] - pix1[0]
-
-    if pix1[1] > pix2[1]:
-        pix1[1] = pix1[1] - pix2[1]
-    else:
-        pix1[1] = pix2[1] - pix1[1]
-
-    if pix1[2] > pix2[2]:
-        pix1[2] = pix1[2] - pix2[2]
-    else:
-        pix1[2] = pix2[2] - pix1[2]
-
-FPS = 1
+FPS = 5
 
 vidcap = VideoCapture('video.mp4')
 success, image = vidcap.read()
@@ -29,7 +13,7 @@ height = int(vidcap.get(3))
 fourcc = VideoWriter_fourcc(*'XVID')
 videoWriter = VideoWriter('output.avi', fourcc, float(FPS), (height, width))
 count = 0
-max_count = 10
+max_count = 100
 last_frames = []
 
 number_of_threads = 1
@@ -37,31 +21,18 @@ thread_span = int(width / number_of_threads)
 print('width =', width)
 print('height =', height)
 
-def for_thread(image, last_frame, index, check_list):
-    def result():
-        start_x = thread_span * index
-        for x in range(start_x, start_x + thread_span):
-            for y in range(height):
-                pixel_difference(image[x][y], last_frame[x][y])
-        check_list[index] = True
-
-    return result
-    
-
 def calculate_frame(image):
     global last_frames
+    image = np.matrix.flatten(image)
+    # image = np.int8(image)
     image_copy = deepcopy(image)
-    if len( last_frames ) >= 1:
+    if len(last_frames) >= 1:
         last_frame = last_frames[-1]
-        check_list = [False] * number_of_threads
-        for index in range(number_of_threads):
-            thread = threading.Thread(target=for_thread(image, last_frame, index, check_list))
-            thread.setDaemon(True)
-            thread.start()
-        while not all(check_list): 
-            pass
+        image = np.subtract(np.maximum(image, last_frame), np.minimum(image, last_frame))
         last_frames = last_frames[1:]
     last_frames.append(image_copy)
+    # image = np.uint8(image)
+    image = np.resize(image, (width, height, 3)) 
     videoWriter.write(image)
 
 while success and count < max_count:
