56d6f6ef7c23fdab67af907976863ac5037b248b
Eruvarn <jakobalwall@gmail.com>
2020-12-27 05:06:22 +0100

    Funkar faktiskt ganska bra.

--- main.py
+++ main.py
@@ -6,6 +6,10 @@ from cv2 import VideoWriter, VideoWriter_fourcc, VideoCapture
 from collections import deque
 
 FPS = 5 
+max_count = 500
+frames_to_save = 30 
+saved_frames_frequency = 2
+emphasis_factor = 1.0
 
 vidcap = VideoCapture('video.mp4')
 success, image = vidcap.read()
@@ -14,36 +18,44 @@ height = int(vidcap.get(3))
 fourcc = VideoWriter_fourcc(*'XVID')
 videoWriter = VideoWriter('output.avi', fourcc, float(FPS), (height, width))
 count = 0
-max_count = 100
 last_frames = deque()
-frames_to_save = 30 
 running_total = np.full(width * height * 3, 0)
 
 print('width =', width)
 print('height =', height)
 
-def calculate_frame(image):
+def calculate_frame(image, count):
     global last_frames, running_total
+
     image = np.matrix.flatten(image)
-    image = np.int32(image)
-    image_copy = deepcopy(image)
+    original_image = np.int32(image)
+
+    should_save_frame = count % saved_frames_frequency == 0
+
     if len(last_frames) >= frames_to_save:
         average_frame = running_total / frames_to_save
-        image = np.absolute(np.subtract(image, average_frame))
-        first_frame = last_frames.popleft()
-        running_total = np.subtract(running_total, first_frame)
-    last_frames.append(image_copy)
-    running_total = np.add(running_total, image_copy)
+        image = emphasis_factor * np.absolute(np.subtract(original_image, average_frame))
+
+        if should_save-frame:
+            first_frame = last_frames.popleft()
+            running_total = np.subtract(running_total, first_frame)
+    else:
+        image = original_image
+
+    if should_save_frame:
+        last_frames.append(original_image)
+        running_total = np.add(running_total, original_image)
+
     image = np.uint8(image)
     image = np.resize(image, (width, height, 3)) 
-    # print(image)
     videoWriter.write(image)
 
 while success and count < max_count:
-    calculate_frame(image)
+    calculate_frame(image, count)
     success, image = vidcap.read()
     print(count)
     count += 1
+
 videoWriter.release()
 vidcap.release()
 print('Total count:', count)
