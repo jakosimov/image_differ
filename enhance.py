7a7daf24b472bb1865ae8d29f4c4d5993758201a
Eruvarn <jakobalwall@gmail.com>
2021-01-17 14:23:46 +0100

    Yes.

--- enhance.py
+++ enhance.py
@@ -1,20 +1,30 @@
-
 import numpy as np
 import processer
+from collections import deque
 import sys
 
 file_name = sys.argv[1]
+max_count = -1
+if len(sys.argv) >= 3:
+   max_count = int(sys.argv[2])
 
-input_file = 'diff_' + file_name + '.avi'
+input_file = '' + file_name + '.mp4'
 output_file = 'out_' + file_name + '.avi'
 emphasis_factor = 50.0
-threshold = 0.5
+threshold = 2.0
 neighbour_threshold = 10
 average_threshold = 3.0
 
-processer = processer.Processer(input_file, output_file, max_count=1000)
+frames_to_save = 30
+saved_frames_frequency = 3
+frames_to_skip = 300
+
+processer = processer.Processer(input_file, output_file, max_count=max_count)
 width = processer.width
 height = processer.height
+
+last_frames = deque()
+running_total = np.full((width, height, 3), 0)
 
 def scale_by_factor(image):
     return image * emphasis_factor
@@ -22,11 +32,6 @@ def scale_by_factor(image):
 def to_pixel_form(image):
     image = np.uint32(image)
     return (image[:,:,0] + image[:,:,1] + image[:,:,2]) / 3
-
-with open(file_name + '_total.npy', 'rb') as f:
-    total = np.load(f, allow_pickle=True)
-    total = np.resize(total, (width, height, 3))
-    total = to_pixel_form(total)
 
 def from_pixel_form(image):
     image = np.minimum(image, 255)
@@ -36,16 +41,6 @@ def from_pixel_form(image):
 
     return image
 
-average_total = np.mean(total)
-total = total * (total >= (average_total * average_threshold))
-# total = from_pixel_form(total)
-
-
-total_difference = np.full((width, height), 1) 
-
-print('width =', width)
-print('height =', height)
-
 def get_sum_of_neighbours(image):
     bot = np.pad(image[1:,:], ((0, 1), (0, 0)), constant_values=(0))
     top = np.pad(image[:-1,:], ((1, 0), (0, 0)), constant_values=(0))
@@ -53,36 +48,73 @@ def get_sum_of_neighbours(image):
     left = np.pad(image[:,:-1], ((0, 0), (1, 0)), constant_values=(0))
     return bot + top + right + left
 
-def apply_threshold(image):
-    return (image >= threshold) * image
-
 def filter_by_neighbours(image):
-    neighbours = get_neighbours(image)
+    neighbours = get_sum_of_neighbours(image)
     return (neighbours >= neighbour_threshold) * image 
 
-def scale_by_running_total(image):
-    global total_difference
-    total_difference = np.add(total_difference, image)
-    return (image / total_difference) * count
+with open(file_name + '_total.npy', 'rb') as f:
+    total = np.load(f, allow_pickle=True)
+    total = np.resize(total, (width, height, 3))
+    total = to_pixel_form(total)
+
+average_total = np.mean(total)
+total = total * (total >= (average_total * average_threshold))
+
+def apply_threshold(image):
+    return (image >= threshold) * image
 
 def filter_out_by_total(image):
     image = image * (total == 0)
     return image
 
-def calc_frame_2(image, count, width, height):
-    
-    # return total 
+last_frame = np.full((width, height), 0)
+def filter_by_last_empty(image):
+    global last_frame
+    temp_frame = image * (last_frame > 0.0)
+    last_frame = image
+    image = temp_frame
+    return image
+
+def filter_by_non_empty_neighbours(image):
+    neigh = get_sum_of_neighbours(image)
+    image = image * (neigh > 0.1)
+    return image
+
+def calculate_frame(image, count, width, height):
+    global last_frames, running_total
+
+    original_image = np.int32(image)
+
+    should_save_frame = count >= frames_to_skip and count % saved_frames_frequency == 0
+
+    if len(last_frames) >= frames_to_save:
+        average_frame = running_total / frames_to_save
+        image = np.absolute(np.subtract(original_image, average_frame))
+
+        if should_save_frame:
+            first_frame = last_frames.popleft()
+            running_total = np.subtract(running_total, first_frame)
+    else:
+        image = np.full((width, height, 3), 0)
+        # should_save_frame = True
+
+    if should_save_frame:
+        last_frames.append(original_image)
+        running_total = np.add(running_total, original_image)
+
     image = to_pixel_form(image)
 
     image = filter_out_by_total(image)
-    # neigh_sum = get_sum_of_neighbours(image)
-    # image = image + neigh_sum
-    # image = apply_threshold(image)
-    # image = filter_by_neighbours(image)
+    image = apply_threshold(image)
+
+    image = filter_by_neighbours(image)
+
     image = scale_by_factor(image)
+    image = filter_by_last_empty(image)
 
     image = from_pixel_form(image)
 
     return image
 
-processer.run(calc_frame_2)
+if __name__ == "__main__":
+    processer.run(calculate_frame)
--- test.py
+++ test.py
@@ -1,18 +1,21 @@
+
 import numpy as np
 
-
-def filter_by_neighbours(image):
+def get_sum_of_neighbours(image):
     bot = np.pad(image[1:,:], ((0, 1), (0, 0)), constant_values=(0))
     top = np.pad(image[:-1,:], ((1, 0), (0, 0)), constant_values=(0))
     right = np.pad(image[:,1:], ((0, 0), (0, 1)), constant_values=(0)) 
     left = np.pad(image[:,:-1], ((0, 0), (1, 0)), constant_values=(0))
     return bot + top + right + left
 
+def filter_by_non_empty_neighbours(image):
+    neigh = get_sum_of_neighbours(image)
+    image = image * (neigh > 0.1)
+    return image
 
 test_data = np.array(
-  [[1, 2, 3],
-   [4, 5, 6],
-   [7, 8, 9]]
+  [[1, 1, 3],
+   [0, 5, 0],
+   [7, 0, 9]]
 )
-
-print(filter_by_neighbours(test_data))
+print(filter_by_non_empty_neighbours(test_data))
